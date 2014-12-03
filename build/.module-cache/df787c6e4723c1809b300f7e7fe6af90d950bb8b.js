b61229f1c01444843c20bf82675f49b4ce329fe0
SimBoo <thisiswoa@163.com>
2014-12-03 15:36:04 +0800

    使用jsx(node下)在本地预编译

---
+++ build/.module-cache/df787c6e4723c1809b300f7e7fe6af90d950bb8b.js
@@ -0,0 +1,4 @@
+React.render(
+  React.createElement("h1", null, "你好!我是华仔！"),
+  document.getElementById('example')
+);
---
+++ src/helloworld.js
@@ -0,0 +1,4 @@
+React.render(
+  <h1>你好!我是华仔！</h1>,
+  document.getElementById('example')
+);
